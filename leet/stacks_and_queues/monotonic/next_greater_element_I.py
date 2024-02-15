35d0718edb6debf20c3d5a5bf8baa9dc08dd8d18
Maciej <maciek.g95@interia.pl>
2024-02-15 20:15:39 +0100

    next greater element

--- leet/stacks_and_queues/monotonic/next_greater_element_I.py
+++ leet/stacks_and_queues/monotonic/next_greater_element_I.py
@@ -7,9 +7,34 @@ input1 = [4,1,2]
 input2 = [1,3,4,2]
 
 
-stack = 1-0, 2-3, 3-1, 4-2
+# zrobic hashmape
+# iterowac po nums1
+# znalesc element w hashmapie i jego index
+# od tego indexu + 1 zaczac iterowac po num2
+# jesli element znajdzie sie wiekszy to dodac do answer
+# jesli nic sie nie znajdzie to -1
 
 
-def nextGreaterElement(nums1: List[int], nums2: List[int]) -> List[int]:
+def next_greater_element(nums1: List[int], nums2: List[int]) -> List[int]:
     answer = []
-    stack 
+    hashMap = {}
+
+    for index, x in enumerate(nums2):
+        hashMap[x] = index
+
+    for x in nums1:
+        wasAppended = False
+        elementValue = hashMap[x]
+        for index in range(elementValue, len(nums2), 1):
+            value = nums2[index]
+            if value > x:
+                answer.append(value)
+                wasAppended = True
+                break
+        if wasAppended == False:
+            answer.append(-1)
+    return answer
+
+
+result = next_greater_element(input1, input2)
+print('test')
